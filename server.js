05bc43de29bdf21b00bf8794955ca3fe3c21d966
alexwc2002 <91438483+alexwc2002@users.noreply.github.com>
2024-04-15 12:31:07 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongodb = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+const MONGO_URL = 'mongodb://localhost:27017/freeway-db';
+const app = express();
+const jsonParser = bodyParser.json();
+
+
+app.use(express.static('test'));
+
+let db = null;
+
+async function startDbAndServer() {
+	// Starts the MongoDB server, and listens for connections
+	
+	client = await mongodb.connect(MONGO_URL);
+	db = client.db('freeway-db');
+	await app.listen(3000);
+	console.log('Listening on port 3000');
+
+};
+
+startDbAndServer();
+
+//async function onSaveCard(req, res) {
+//	const card = req.body; // Gets the body (Card details)
+//	const collection = db.collection('card'); 
+//	const result = await collection.insertOne(card); // Saves the card
+//	const cardID = result.insertedId; // Gets the ObjectId of card
+//	res.json({ cardId: cardID }); // Sends the ObjectId in the JSON response
+//}
+//app.post('/save', jsonParser, onSaveCard);
+
+/*
+ * Complete the onGetCard function, which takes in an HTTP request 'req'.
+ * 'req' is sent when _loadCard() in "public/js/card-view.js" is executed
+ * or when a URL (e.g., http://localhost:3000/id/5bbb8a07ebbf6a9cf4d839f5)
+ * is entered in your browser. The request sends a cardID to the Node server.
+ * The cardID is also a document ID in MongoDB.
+ *
+ * After receiving the request, the Node server should search the cardID in
+ * the 'card' collection and return the content of the stored document matching
+ * the cardID to the browser.
+ */ 
+
+//async function onGetCard(req, res) {
+//	return;
+//	const cardID = req.params.cardId; // Gets the cardId (ObjectId)
+//	const collection = db.collection('card');
+//	const query = { _id: ObjectId(cardID) }; // Constructs the query
+//	const card = await collection.findOne(query); // Gets the associated card
+//	res.json(card); // Sends the card in the JSON response
+//}
+//app.get('/get/:cardId', onGetCard);
+//
+//async function onGetCardView(req, res) {
+//  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+//}
+//app.get('*', onGetCardView);
