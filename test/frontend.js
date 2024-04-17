5723a883386a6c0b3218baaf263700aaa91ca2db
Alex <alexander.curran@wsu.edu>
2024-04-16 22:03:11 -0700

    Added data

--- test/frontend.js
+++
@@ -1,8 +0,0 @@
-function ChangeLocationSubmit(){
-	const startInput = document.getElementById("startInput")
-	const endInput = document.getElementById("endInput")
-	const stationInput = document.getElementById("stationInput")
-	//call .value on const to get the information or else it will just be an html Element
-	if(startInput.value.length)
-	document.getElementById("temp").textContent = startInput.value.length
-}
