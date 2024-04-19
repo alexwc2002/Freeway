86820197c55244e1683311bbe765e2997845b112
Alex <alexander.curran@wsu.edu>
2024-04-19 14:06:15 -0700

    Front end baby

---
+++ public/compute.js
@@ -0,0 +1,32 @@
+class ComputeScreen {
+	constructor() {
+		
+		this.averageTravelTimeSubmit = this.averageTravelTimeSubmit.bind(this);
+
+		document.getElementById('compute').addEventListener('submit', this.averageTravelTimeSubmit);
+	}
+
+		
+	async averageTravelTimeSubmit(event){
+		event.preventDefault();
+
+		const formData = new FormData(event.target);
+
+		console.log(event.target);
+
+		const params = new URLSearchParams(formData).toString();
+
+		console.log(params);
+
+		const result = await fetch('/traveltime?' + params);
+
+		const json = await result.json();
+
+		console.log(json.time);
+		
+		document.getElementById('travelTime').textContent = json.time;	
+		
+	}
+}
+
+new ComputeScreen();
--- server.js
+++ server.js
@@ -29,56 +29,27 @@ async function startDbAndServer() {
 
 startDbAndServer();
 
-async function helloworld(req, res) {
-	console.log(req.query.name);
-	const response = { message: "Hello " + req.query.name + "!" };
-	res.json(response);
-}
-
-app.get('/hello', jsonParser, helloworld); 
-
 async function getTravelTime(req, res) {
 	const start = Number(req.query.start);
 	const end = Number(req.query.end);
 	const result = end - start;
 	const response = { time: result };
 	res.json(response);
-
 }
 
 app.get('/traveltime', jsonParser, getTravelTime);
-//async function onSaveCard(req, res) {
-//	const card = req.body; // Gets the body (Card details)
-//	const collection = db.collection('card'); 
-//	const result = await collection.insertOne(card); // Saves the card
-//	const cardID = result.insertedId; // Gets the ObjectId of card
-//	res.json({ cardId: cardID }); // Sends the ObjectId in the JSON response
-//}
-//app.post('/save', jsonParser, onSaveCard);
 
-/*
- * Complete the onGetCard function, which takes in an HTTP request 'req'.
- * 'req' is sent when _loadCard() in "public/js/card-view.js" is executed
- * or when a URL (e.g., http://localhost:3000/id/5bbb8a07ebbf6a9cf4d839f5)
- * is entered in your browser. The request sends a cardID to the Node server.
- * The cardID is also a document ID in MongoDB.
- *
- * After receiving the request, the Node server should search the cardID in
- * the 'card' collection and return the content of the stored document matching
- * the cardID to the browser.
- */ 
 
-//async function onGetCard(req, res) {
-//	return;
-//	const cardID = req.params.cardId; // Gets the cardId (ObjectId)
-//	const collection = db.collection('card');
-//	const query = { _id: ObjectId(cardID) }; // Constructs the query
-//	const card = await collection.findOne(query); // Gets the associated card
-//	res.json(card); // Sends the card in the JSON response
-//}
-//app.get('/get/:cardId', onGetCard);
-//
-//async function onGetCardView(req, res) {
-//  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-//}
-//app.get('*', onGetCardView);
+async function update(req, res) {
+	const body = req.body;
+	
+	console.log(body);
+
+	const response = { test: "Test" };
+
+	res.json(response);
+
+}
+
+app.post('/updatestation', jsonParser, update)
+
