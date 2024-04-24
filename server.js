d90e6bfc3841be9b5d1d64764006014880ade2d6
JohnnyFlint <115769806+JohnnyFlint@users.noreply.github.com>
2024-04-24 12:53:24 -0700

    Update Server

--- server.js
+++ server.js
@@ -17,13 +17,12 @@ async function startDbAndServer() {
 	// Starts the MongoDB server, and listens for connections
 	
 	client = await mongodb.connect(MONGO_URL);
-	db = client.db('freeway');
+	db = client.db('Freeway');
 	
 	await app.listen(3000);
 	let collection = db.collection('stations')
 	console.log('Listening on port 3000');
 	
-	//changeStationName()
 };
 
 startDbAndServer();
@@ -38,11 +37,13 @@ async function getTravelTime(req, res) {
 	let start = req.query.start
 	let end = req.query.end;
 	console.log(loc,start,end)
+	//Query to find all detectors with our location
 	let stationIDs = await collectionDetectors.find({locationtext: loc}).toArray()
 	if(stationIDs.length === 0 ){
 		console.log("No detectors found with that location")
 		return 
 	}
+	// put all of the detecor ids into an array
 	let detectorIDs = []
 	let xx = 0 
 	while(xx < stationIDs.length){
@@ -54,6 +55,7 @@ async function getTravelTime(req, res) {
 	let totalSpeed = 0
 	let countSpeed = 0
 	let len = 0 
+	//loop through all detecors 
 	while(xx < detectorIDs.length){
 		let volume = await collection.find({starttime: {$gte:start,$lte:end},detectorid: detectorIDs[xx]}).toArray()
 		console.log("volume.find:",volume)
@@ -62,19 +64,22 @@ async function getTravelTime(req, res) {
 		console.log("station.find",stations)
 		len = stations[0].length
 		console.log("len:",len)
+		console.log("volume:",volume)
 		if(volume.length === 0 ) break
+		// get the volume 
 		while(yy < volume.length){
 			if(volume[yy].volume === undefined)
 			{ 
-			// console.log("no element volume")
+			console.log("no element volume")
 			}
 			else
 			{	
 				console.log("volume:",volume[yy].volume)
 				totalVolume+=volume[yy].volume
 			}
+			//get the spped 
 			if(volume[yy].speed === undefined){
-				// console.log("no element speed")
+				console.log("no element speed")
 			}else{
 				console.log("speed",volume[yy].speed)
 				totalSpeed += volume[yy].speed
@@ -84,6 +89,7 @@ async function getTravelTime(req, res) {
 		}
 		xx+=1
 	}
+	//calulations 
 	console.log("total Speed", totalSpeed)
 	console.log("counts of speed inputs",countSpeed)
 	console.log("avg speed:",totalSpeed/countSpeed)
@@ -91,12 +97,13 @@ async function getTravelTime(req, res) {
 	let travelTimeSeconds = len/avgSpeed * 3600
 	console.log("Travel time in seconds:",travelTimeSeconds)
 	console.log("Total Volume:", totalVolume)
-	// else console.log(results)
+	
 	if(isNaN(travelTimeSeconds)){
 		console.log("error")
 		travelTimeSeconds = "Input Error"
 		totalVolume = ""
 	}
+	//return calulations or error 
 	const response = { time: travelTimeSeconds, volume: totalVolume };
 	res.json(response);
 }
@@ -135,6 +142,7 @@ async function update(req, res) {
 	{ 
 	console.log("results for detectors:",resultsDetectors)
 	}
+	//modifiedCount is the amount of elements in the table changed
 	let resultsStations = await stations.updateMany(filter,update)
 	if(resultsStations.modifiedCount===0){ 
 	console.log("no updates made to stations collection")
@@ -149,99 +157,3 @@ async function update(req, res) {
 }
 
 app.post('/updatestation', jsonParser, update)
-
-async function calculateTimeandVolume(){
-	let collection = db.collection('loopData')
-	let collectionDetectors = db.collection('detectors')
-	let collectionStation = db.collection('stations')
-	let loc = "Sunnyside NB"
-	let stationIDs = await collectionDetectors.find({locationtext: loc}).toArray()
-	if(stationIDs.length === 0 ){
-		console.log("No detectors found with that location")
-		return 
-	}
-	let detectorIDs = []
-	let xx = 0 
-	while(xx < stationIDs.length){
-		detectorIDs.push(stationIDs[xx].detectorid)
-		xx+=1
-	}
-	xx = 0
-	let totalVolume = 0
-	let totalSpeed = 0
-	let countSpeed = 0
-	let len = 0 
-	while(xx < detectorIDs.length){
-		let volume = await collection.find({starttime: {$gte:start,$lte:end},detectorid: detectorIDs[xx]}).toArray()
-		let yy = 0 
-		let stations = await collectionStation.find({locationtext: loc}).toArray()
-		len = stations[0].length
-		while(yy < volume.length){
-			if(volume[yy].volume === undefined)
-			{ 
-			// console.log("no element volume")
-			}
-			else
-			{
-				totalVolume+=volume[yy].volume
-			}
-			if(volume[yy].speed === undefined){
-				// console.log("no element speed")
-			}else{
-				// console.log("speed",volume[yy].speed)
-				totalSpeed += volume[yy].speed
-				countSpeed += 1
-			}
-			yy+=1
-		}
-		xx+=1
-	}
-	// console.log("total Speed", totalSpeed)
-	// console.log("counts of speed inputs",countSpeed)
-	// console.log("avg speed:",totalSpeed/countSpeed)
-	let avgSpeed = totalSpeed/countSpeed
-	let travelTimeSeconds = len/avgSpeed * 3600
-	console.log("Travel time in seconds:",travelTimeSeconds)
-	console.log("Total Volume:", totalVolume)
-	// else console.log(results)
-	
-}
-
-async function changeStationName(){
-	let detectors = db.collection('detectors')
-	let stations = db.collection('stations')
-	let stationID = 1045
-	let newName = "Sunnyside NB"
-	let oldName = "YOOO"
-	// Check to see if the new name is already in the DB 
-	let stationInfo = await stations.find({locationtext: newName}).toArray()
-	if(stationInfo.length >= 1){
-		console.log("new name is already in the DB")
-		return
-	}
-	//filter is what we are looking to Update
-	//update is what we are looking to update it with 
-	let filter = {stationid:stationID , locationtext: oldName }
-	let update = {$set:{locationtext: newName }}
-	let resultsDetectors = await detectors.updateMany(filter,update)
-	
-	if(resultsDetectors.modifiedCount===0) 
-	{
-		console.log("no updates made to detectors collection")
-		return
-	}
-	else
-	{ 
-	console.log("results for detectors:",resultsDetectors)
-	}
-	let resultsStations = await stations.updateMany(filter,update)
-	if(resultsStations.modifiedCount===0){ 
-	console.log("no updates made to stations collection")
-	}
-	else{ 
-	console.log("results for detectors:",resultsDetectors)
-	}
-
-	
-}
-
